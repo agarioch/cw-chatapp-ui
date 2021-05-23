4797a30371e39a69dd59a1e3f7ae0eefabc60267
agarioch <alistairgarioch@gmail.com>
2021-05-23 09:24:41 +0100

    cleanup function names and remove console logs

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-// Placenames from Index of Place Names in Great Britain (July 2016), Office for National Statistics
-$.support.cors = true
-
-const username = 'You'
-const botname = 'CountryBot'
+const USERNAME = 'You'
+const BOTNAME = 'CountryBot'
 const messages = $('#messages')
 const messagebox = $('#messages-window')
 const countryNames = countries.map((e) => e.Name.toLowerCase())
@@ -10,20 +7,20 @@ const letters = Array.from('abcdefghijklmnopqrstuvwxyz')
 let currentLetter = 0
 
 $(document).ready(function () {
-    welcome()
+    appendWelcomeMessage()
 })
 
 $("form[name='message-form']").submit(function (event) {
     event.preventDefault()
     const msg = $(this).find('input[name="input-message"]').val()
     if (msg) {
-        appendMessage(msg, username)
-        setTimeout(reply, 300, msg)
+        appendMessage(msg, USERNAME)
+        setTimeout(appendBotReply, 300, msg)
         this.reset()
     }
 })
 
-function reply(msg) {
+function appendBotReply(msg) {
     const lowerMsg = msg.toLowerCase().trim()
     if (countryNames.includes(lowerMsg)) {
         if (lowerMsg.charAt(0) === letters[currentLetter]) {
@@ -31,7 +28,7 @@ function reply(msg) {
             appendMessage(
                 randomCountry(letters[currentLetter]) +
                     nextLetterMessage(currentLetter),
-                botname
+                BOTNAME
             )
             currentLetter = nextLetter(currentLetter)
         } else {
@@ -39,7 +36,7 @@ function reply(msg) {
                 `Sorry, ${msg} doesn't begin with ${letters[
                     currentLetter
                 ].toUpperCase()}`,
-                botname
+                BOTNAME
             )
         }
     } else {
@@ -47,12 +44,12 @@ function reply(msg) {
             `Sorry I can't find "${msg}" on the iso country list. Try to name a country starting with letter ${letters[
                 currentLetter
             ].toUpperCase()}`,
-            botname
+            BOTNAME
         )
     }
 }
 
-function scrollWindow() {
+function scrollToLastMessage() {
     messagebox
         .get(0)
         .scroll({ left: 0, top: messages.height(), behavior: 'smooth' })
@@ -70,50 +67,37 @@ function nextLetterMessage(n) {
     if (n + 1 === letters.indexOf('x')) {
         currentLetter = nextLetter(currentLetter)
         currentLetter = nextLetter(currentLetter)
-        return `. There are no X countries, let's go to ${letters[
-            currentLetter + 1
-        ].toUpperCase()}.`
+        return `. There are no X countries, let's go to Y.`
     } else {
         return `, now we are on ${letters[currentLetter + 1].toUpperCase()}...`
     }
 }
 
-function welcome() {
+function appendWelcomeMessage() {
     const welcome =
         "Hi, I'm CountryBot. Name a country beginning with the letter A and I'll reply with one beginning with letter B and so on down the alphabet."
-    appendMessage(welcome, botname)
+    appendMessage(welcome, BOTNAME)
 }
 
 function randomCountry(letter) {
-    if (!letter) {
+    const validCountries = countryNames.filter((c) => c.charAt(0) === letter)
+    if (validCountries.length > 0) {
         return cleanCountryName(
-            countryNames[Math.floor(Math.random() * countryNames.length)]
+            validCountries[Math.floor(Math.random() * validCountries.length)]
         )
     } else {
-        const validCountries = countryNames.filter(
-            (c) => c.charAt(0) === letter
-        )
-        console.log(validCountries)
-        if (validCountries.length > 0) {
-            return cleanCountryName(
-                validCountries[
-                    Math.floor(Math.random() * validCountries.length)
-                ]
-            )
-        } else {
-            return `Let's skip ${letters[currentLetter].toUpperCase()}`
-        }
+        return `Let's skip ${letters[currentLetter].toUpperCase()}`
     }
 }
 
 function cleanCountryName(str) {
-    console.log(typeof str)
+    // capitalize first letter of every word
     return str.replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase())
 }
 
 function appendMessage(text, user) {
     const time = new Date().toLocaleString('en-GB')
-    const msg = $(`<div class="${user === botname ? '' : 'user'} message-line">
+    const msg = $(`<div class="${user === BOTNAME ? '' : 'user'} message-line">
         <div class="message">
             <span class="metadata">
                 <h3>${user}</h3>
@@ -123,5 +107,5 @@ function appendMessage(text, user) {
         </div>
     </div>`)
     messages.append(msg)
-    scrollWindow()
+    scrollToLastMessage()
 }
